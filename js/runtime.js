85b0733765ce699b10bc2cb2a778be39f17cbe18
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2023-08-04 11:31:02 +0800

    Site updated: 2023-08-04 11:31:00

--- js/runtime.js
+++ js/runtime.js
@@ -1,6 +1,6 @@
 var now = new Date();
 function createtime() {
-    var grt= new Date("08/03/2023 22:05:00");//在此处修改你的建站时间，格式：月/日/年 时:分:秒
+    var grt= new Date("08/03/2023 21:26:32");//在此处修改你的建站时间，格式：月/日/年 时:分:秒
     now.setTime(now.getTime()+250);
     days = (now - grt ) / 1000 / 60 / 60 / 24; dnum = Math.floor(days);
     hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours);
@@ -8,7 +8,7 @@ function createtime() {
     mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;}
     seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
     snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;}
-    document.getElementById("timeDate").innerHTML = "💻 本站已运行 "+dnum+" 天";
+    document.getElementById("timeDate").innerHTML = "📢 本站已运行 "+dnum+" 天";
     document.getElementById("times").innerHTML = hnum + " 时 " + mnum + " 分 " + snum + " 秒";
 }
 setInterval("createtime()",250);
