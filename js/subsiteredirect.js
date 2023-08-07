64264edb0e112b9383b3c02c996ab681cd3a2bdc
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2023-08-07 14:40:56 +0800

    Site updated: 2023-08-07 14:40:54

---
+++ js/subsiteredirect.js
@@ -0,0 +1,15 @@
+function redir2gh () {
+    window.location.href = "https://wanghaonie.github.io" + window.location.pathname;
+  }
+
+function redir2lan () {
+    window.location.href = "http://10.0.0.10:4000" + window.location.pathname;
+  }
+
+function redir2v () {
+    window.location.href = "https://wanghaonie.vercel.app" + window.location.pathname;
+  }
+
+function redir2n () {
+    window.location.href = "http://wanghaonie.netlify.app" + window.location.pathname;
+  }
