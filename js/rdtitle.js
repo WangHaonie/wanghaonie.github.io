f970d91fc4e4444bf2a4873a5979f6dfcc05f1e3
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2023-08-07 20:28:30 +0800

    Site updated: 2023-08-07 20:28:28

--- js/rdtitle.js
+++ js/rdtitle.js
@@ -3,12 +3,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/img/favicon.png");
-        document.title = '人怎么走掉了啦啊喂 !? | ' + OriginTitle;
+        document.title = '🥺人怎么走掉了啦啊喂 !? | ' + OriginTitle;
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/img/favicon.png");
-        document.title = '嗯嗯, 欢迎回来~ | ' + OriginTitle;
+        document.title = '🥰嗯嗯, 欢迎回来~ | ' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
--- js/runtime.js
+++ js/runtime.js
@@ -8,7 +8,7 @@ function createtime() {
     mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;}
     seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
     snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;}
-    document.getElementById("timeDate").innerHTML = "📢 本站已悄无声息地运行了 "+dnum+" 天";
+    document.getElementById("timeDate").innerHTML = "本站已悄无声息地运行了 "+dnum+" 天 ";
     document.getElementById("times").innerHTML = hnum + " 时 " + mnum + " 分 " + snum + " 秒";
 }
 setInterval("createtime()",250);
