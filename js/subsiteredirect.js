ca258f1831cc55eac43962018f200c44844e1fbd
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2025-07-24 18:26:09 +0800

    Site updated: 2025-07-24 18:26:07

--- js/subsiteredirect.js
+++ js/subsiteredirect.js
@@ -1,21 +1,15 @@
-/* WangHaonie 亲手打造，用于跳转到到对应分站 - v1.0.0*/
-
-/* 跳转到到 GitHub Pages */
 function redir2gh() {
-    window.location.href = "https://wanghaonie.github.io" + window.location.pathname;
-  }
+  window.location.href = "https://wanghaonie.github.io" + window.location.pathname;
+}
 
-/* 跳转到到 本地局域网 */
 function redir2lan() {
-    window.location.href = "http://192.168.80.132:4000" + window.location.pathname;
-  }
+  window.location.href = "http://192.168.80.134:4000" + window.location.pathname;
+}
 
-/* 跳转到到 Vercel */
 function redir2v() {
-    window.location.href = "https://wanghaonie.vercel.app" + window.location.pathname;
-  }
+  window.location.href = "https://wanghaonie.vercel.app" + window.location.pathname;
+}
 
-/* 跳转到到 Netlify */
 function redir2n() {
-    window.location.href = "http://wanghaonie.netlify.app" + window.location.pathname;
-  }
+  window.location.href = "http://wanghaonie.netlify.app" + window.location.pathname;
+}
