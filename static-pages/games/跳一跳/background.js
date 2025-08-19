81e99413ebfbf35e4fbd6d9945c52776cb956cb0
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2025-08-19 17:50:07 +0800

    Site updated: 2025-08-19 17:50:06

--- static-pages/games/跳一跳/background.js
+++
@@ -1,2 +0,0 @@
-(function(){function Background(){var ctx=window.ctx;var canvas=window.canvas;var img=window.img;this.draw=draw;function draw(){ctx.drawImage(img.imgObject.background,0,0,canvas.width,canvas.height);}}
-game.background=new Background();})();
